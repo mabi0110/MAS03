a625afbd5e9ee6869b8ac5af5da5971a049354c4
MaBi0110 <biziewskamk@gmail.com>
2023-05-29 18:59:01 +0200

    małe poprawki

--- src/Main.java
+++ src/Main.java
@@ -10,6 +10,8 @@ public class Main {
     public static void main(String[] args) {
         /*
 
+        //klasa abstrakcyjna i polimorfizm
+
         Publication book1 = new Book("Ania z Zielonego Wzgórza", Year.of(2001), "L. Montgomery", "123456789");
         Publication book2 = new Book("Janko Muzykant", Year.of(2003), "M. Konopnicka", "123456780");
         Publication magazine1 = new Magazine("Ilustrator", Year.of(2001), 12, 1);
@@ -23,7 +25,11 @@ public class Main {
             System.out.println(publication.getData());
         }
 
-         */
+        */
+
+        /*
+
+        // wielodziedziczenie
 
         LibrarianAdmin librarianAdmin = new LibrarianAdmin("Jan", "Nowak", Year.of(1992), "e12345", "888123123");
         System.out.println(librarianAdmin.getSalary());
@@ -32,5 +38,7 @@ public class Main {
         librarianAdmin.addPerson(new User("Ola", "Nowak", Year.of(2003), "u12345"));
         librarianAdmin.addPerson(new Admin("Adam", "Nowak", Year.of(2000), "e12346", "789123456"));
         librarianAdmin.addPerson(new Librarian("Ela", "Nowak", Year.of(1999), "e12347"));
+
+        */
     }
 }
